73010fa255f84c005bea393df1e432a2e7f6b4bb
Singh <singh2rajiv@dgb-c02vh08bh.lan>
2020-06-05 15:24:19 -0700

    Updated the test with custom functions

--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -47,4 +47,21 @@ describe('Mocha Steps Demo', () => {
         const signInButton = await page.isElementVisible('#signin_button');
         expect(signInButton).to.be.true;
     });
+
+    step('Should display login form', async () => {
+        await page.waitAndClick('#signin_button');
+        const loginForm = await page.isElementVisible('#login_form');
+        expect(loginForm).to.be.true;
+        const signInButton = await page.isElementVisible('#signin_button');
+        expect(signInButton).to.be.false;
+    });
+
+    step('Should login to the application', async () => {
+        await page.waitAndType('#user_login', "username");
+        await page.waitAndType('#user_password', "password");
+        await page.waitAndClick('.btn-primary');
+        const navbar = await page.isElementVisible('.nav-tabs');
+        expect(navbar).to.be.true;
+
+    })
 })
